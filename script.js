dd0005fc856506d2bc232c9eefa1f6224d8db3ec
Juan <juanestebanpiconlopez10@gmail.com>
2026-04-05 21:56:56 -0500

    script y borde email

--- script.js
+++ script.js
@@ -1,33 +1,42 @@
-const inputEmail = document.getElementById('user-email');
-const boton = document.getElementById('btn-enviar');
+const inputEmail = document.getElementById("user-email");
+const boton = document.getElementById("btn-enviar");
+const mensajeFeedback = document.getElementById("mensaje-feedback");
 const urlDestino = "https://webhook.site/ea0d37d9-e386-47fe-93cc-1043ad80fd62";
 
-boton.addEventListener('click', function(event){
-    
-    event.preventDefault();
-    
-    const correo = inputEmail.value;
+boton.addEventListener("click", function (event) {
+  event.preventDefault();
 
-    if (correo.includes('@')) {
-        console.log("Enviando correo: " + correo);
+  const correo = inputEmail.value;
 
-        fetch(urlDestino, {
-            method: 'POST',
-            mode: 'no-cors',
-            body: JSON.stringify({ 
-                email_cliente: correo, 
-                proyecto: "HELMI"
-            })
-        })
-        .then(() => {
-            console.log("Petición enviada.");
-            alert("¡Pronto nos contactaremos con vos!");
-        })
-        .catch(error => {
-            console.error("Error en el envío:", error);
-        });
+  if (correo.includes("@")) {
+    console.log("Enviando correo: " + correo);
 
-    } else {
-        alert("Por favor, ingresa un correo válido.");
-    }
+    fetch(urlDestino, {
+      method: "POST",
+      mode: "no-cors",
+      body: JSON.stringify({
+        email_cliente: correo,
+        proyecto: "HELMI",
+      }),
+    })
+      .then(() => {
+        mensajeFeedback.innerText = "¡Pronto nos contactaremos con vos!";
+        inputEmail.value = "";
+      })
+      .catch((error) => {
+        console.error("Error en el envío:", error);
+      });
+  } else {
+    alert("Por favor, ingresa un correo válido.");
+  }
 });
+
+const barraBusqueda = document.getElementById("barra-busqueda");
+function cambiarplace(texto) {
+  barraBusqueda.placeholder = texto;
+}
+barraBusqueda.addEventListener("focus", () => cambiarplace("¿Qué necesitas?"));
+
+barraBusqueda.addEventListener("blur", () =>
+  cambiarplace("Servicios, Hogar, freelancers..."),
+);
