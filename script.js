93552decc11f7e919356b7e4b21bbf9278e23e65
Juan <juanestebanpiconlopez10@gmail.com>
2026-05-25 12:49:02 -0500

    removiendo instalador pesado y subiendo proyecto

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const inputEmail = document.getElementById("user-email");
 const boton = document.getElementById("btn-enviar");
 const mensajeFeedback = document.getElementById("mensaje-feedback");
-const urlDestino = "https://webhook.site/ea0d37d9-e386-47fe-93cc-1043ad80fd62";
+const urlDestino = "https://proud-island-93.webhook.cool";
 
 boton.addEventListener("click", function (event) {
   event.preventDefault();
@@ -9,8 +9,6 @@ boton.addEventListener("click", function (event) {
   const correo = inputEmail.value;
 
   if (correo.includes("@")) {
-    console.log("Enviando correo: " + correo);
-
     fetch(urlDestino, {
       method: "POST",
       mode: "no-cors",
@@ -27,7 +25,6 @@ boton.addEventListener("click", function (event) {
         console.error("Error en el envío:", error);
       });
   } else {
-    alert("Por favor, ingresa un correo válido.");
   }
 });
 
@@ -40,3 +37,36 @@ barraBusqueda.addEventListener("focus", () => cambiarplace("¿Qué necesitas?"))
 barraBusqueda.addEventListener("blur", () =>
   cambiarplace("Servicios, Hogar, freelancers..."),
 );
+
+const eyes = document.querySelectorAll(".eye");
+
+window.addEventListener("mousemove", (e) => {
+  eyes.forEach((eye) => {
+    const pupil = eye.querySelector(".pupil");
+    const eyeRect = eye.getBoundingClientRect();
+
+    const eyeCenterX = eyeRect.left + eyeRect.width / 2;
+    const eyeCenterY = eyeRect.top + eyeRect.height / 2;
+
+    const deltaX = e.clientX - eyeCenterX;
+    const deltaY = e.clientY - eyeCenterY;
+
+    const angle = Math.atan2(deltaY, deltaX);
+
+    const maxMoveX = 6;
+    const maxMoveY = 4;
+
+    const moveX = Math.cos(angle) * Math.min(Math.abs(deltaX / 15), maxMoveX);
+    const moveY = Math.sin(angle) * Math.min(Math.abs(deltaY / 15), maxMoveY);
+
+    pupil.style.transform = `translate(calc(-50% + ${moveX}px), calc(-50% + ${moveY}px))`;
+  });
+});
+
+function scrollCategorias(amount) {
+  const container = document.getElementById("categoriaspp");
+  container.scrollBy({
+    left: amount,
+    behavior: "smooth",
+  });
+}
